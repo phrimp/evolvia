723c49cf1fbf8638aca636a26407f360af241ecf
Phrimp <thaiphienn@gmail.com>
2025-08-06 10:23:39 +0700

    fix: gorm syntax

--- services/auth-service/internal/models/profile.go
+++ services/auth-service/internal/models/profile.go
@@ -2,7 +2,7 @@ package models
 
 type UserProfile struct {
 	DisplayName string   `bson:"displayName" json:"displayName"`
-	Role        []string `bson:"-" json:"roles"`
+	Roles       []string `bson:"-" json:"roles"`
 }
 
 type UserWithProfile struct {
